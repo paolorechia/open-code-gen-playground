c44f9a3a085d20326e89a68c177407a321eb367c
Paolo Rechia <paolorechia@gmail.com>
2022-12-18 09:43:50 +0100

    Add 'loading' logic

--- open-code-gen-playground/src/App.js
+++ open-code-gen-playground/src/App.js
@@ -3,15 +3,26 @@ import React, { useRef, useEffect, useState } from "react";
 import Editor from "@monaco-editor/react";
 import { sendPromptQuery } from "./CodeGen.js"
 
+const RequestState = {
+  "ready": 1,
+  "loading": 2
+}
+const RequestClass = {
+  1: "ready",
+  2: "loading"
+}
+
 function App() {
   const editorRef = useRef(null);
   const [editorValue, setEditorValue] = useState("");
+  const [requestState, setRequestState] = useState(RequestState.ready);
 
   useEffect(() => {
     console.log('Editor State', editorValue);
   }, [editorValue])
 
   async function sendPromptAction() {
+    setRequestState(RequestState.loading)
     const result = await sendPromptQuery(editorValue)
     appendResponseToEditor(result)
   }
@@ -19,6 +30,7 @@ function App() {
   function appendResponseToEditor(text) {
     editorRef.current.setValue(editorValue + "\n" + text)
     handleEditorChange()
+    setRequestState(RequestState.ready)
   }
 
   function handleEditorChange() {
@@ -50,7 +62,10 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="editor-wrapper">
+      <div className={`loading-wrapper-${RequestClass[requestState]}`}>
+        Loading
+      </div>
+      <div className={`editor-wrapper editor-wrapper-${RequestClass[requestState]}`}>
         <div className="editor-inner-wrapper">
           <Editor
             height="400px"
--- open-code-gen-playground/src/CodeGen.js
+++ open-code-gen-playground/src/CodeGen.js
@@ -1,6 +1,8 @@
 const API_URL = "http://localhost:3000"
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 export async function sendPromptQuery(prompt) {
+    await delay(2000)
     await fetch(API_URL)
     return "Test prompt result"
 }
