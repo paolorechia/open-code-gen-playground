a245625b98c792243e70e2212b38c65cb4f1bcce
Paolo Rechia <paolorechia@gmail.com>
2022-12-18 09:12:31 +0100

    Basic monaco editor

--- open-code-gen-playground/src/App.js
+++ open-code-gen-playground/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef } from "react";
+import Editor from "@monaco-editor/react";
 
 function App() {
+  const editorRef = useRef(null);
+
+
+  function handleEditorDidMount(editor, monaco) {
+    // here is the editor instance
+    // you can store it in `useRef` for further usage
+    editorRef.current = editor; 
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Open Code Gen Playground
       </header>
+
+      <div className="control-center-wrapper">
+        <div className="control-center-inner-wrapper">
+          <div className="control-center">
+            <button>
+              Send Prompt
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="editor-wrapper">
+        <div className="editor-inner-wrapper">
+          <Editor
+            height="400px"
+            defaultLanguage="python"
+            defaultValue="# some comment"
+            onMount={handleEditorDidMount}
+          />
+        </div>
+      </div>
     </div>
   );
 }
